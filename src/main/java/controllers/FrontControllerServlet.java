434becb142c7321523146d28d1c12d24eedfab75
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-06-11 05:55:17 +0300

    make controller

--- src/main/java/controllers/FrontControllerServlet.java
+++ src/main/java/controllers/FrontControllerServlet.java
@@ -1,5 +1,39 @@
 package main.java.controllers;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class FrontControllerServlet {
     
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+            throws ServletException, IOException {
+        
+        String url = req.getRequestURL().toString();
+        // String query = req.getQueryString();
+        // if (query != null) url += "?" + query;
+        res.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+
+        processRequest(url, out);
+        
+    }
+
+    private void processRequest(String url, PrintWriter out) {
+        out.println("<html><body>");
+        out.println("<h2>URL demandée :</h2>");
+        out.println("<p style='color:blue;font-size:1.2em'>" + url + "</p>");
+        out.println("</body></html>");
+    }
+
+    // @Override
+    // protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+    //         throws ServletException, IOException {
+    
+    // }
+
 }
