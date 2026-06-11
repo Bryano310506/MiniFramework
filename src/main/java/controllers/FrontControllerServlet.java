6c579b754c336a9d5f4a8e14cc1afaa85709ddf7
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-06-11 19:09:24 +0300

    init FrontControllerServlet

--- src/main/java/controllers/FrontControllerServlet.java
+++ src/main/java/controllers/FrontControllerServlet.java
@@ -1,5 +1,35 @@
 package main.java.controllers;
 
-public class FrontControllerServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) 
+            throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+            throws ServletException, IOException {
+        processRequest(req, res);
+    }
+
+    private void processRequest(HttpServletRequest req, HttpServletResponse res) 
+            throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        PrintWriter out = res.getWriter();
+
+        out.println("<html><body>");
+        out.println(uri);
+        out.println("<body><html>");
+    }
     
 }
