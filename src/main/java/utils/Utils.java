839e5f06e022fb9971cd9bf3e1323e4fc4fff4a5
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-06-16 15:09:53 +0300

    init function loadClasses()

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,14 @@
+package main.java.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+    
+    public static List<Class<?>> loadClasses(String packageName, Class<?> annotation) {
+        List<Class<?>> list = new ArrayList<>();
+
+        return list;
+    }
+
+}
