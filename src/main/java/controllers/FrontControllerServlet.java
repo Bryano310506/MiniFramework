a42507952ed0b7169ce25a3ce2ad987231c58b24
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-06-17 18:09:11 +0300

    add init

--- src/main/java/controllers/FrontControllerServlet.java
+++ src/main/java/controllers/FrontControllerServlet.java
@@ -2,14 +2,28 @@ package main.java.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import main.java.annotation.Controller;
+import main.java.utils.Utils;
 
 public class FrontControllerServlet extends HttpServlet {
+    List<String> listClasses;
 
+    public void init() throws ServletException {
+        String initial = this.getInitParameter("Controller");
+        try {
+            listClasses = Utils.intoString(Utils.loadClasses(initial, Controller.class));
+        } catch(Exception e) {
+            e.printStackTrace();
+            listClasses = null;
+        }
+    }
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) 
             throws ServletException, IOException {
@@ -30,7 +44,15 @@ public class FrontControllerServlet extends HttpServlet {
 
         out.println("<html><body>");
         out.println(uri);
+        printClasses(out);
         out.println("<body><html>");
+    }
+
+    private void printClasses(PrintWriter out) {
+        for(String s : this.listClasses) {
+            out.println("\n");
+            out.println(s);
+        }
     }
     
 }
