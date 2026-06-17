9f7693e2f74c6c7017f4fe17fc22a6caeb9e4ce3
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-06-17 18:10:01 +0300

    add Annotation Controller

--- src/main/java/annotation/Controller.java
+++ src/main/java/annotation/Controller.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Controller {
-    
+    public String value() default "Hello from Controller Annotation";
 }
