7f76afa336f7745cc829354711a4c11d2250e4d2
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-06-23 06:28:25 +0300

    integration du affichage method

--- src/main/java/controllers/FrontControllerServlet.java
+++ src/main/java/controllers/FrontControllerServlet.java
@@ -2,22 +2,29 @@ package main.java.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import main.java.annotation.Controller;
-import main.java.utils.Utils;
+import main.java.annotation.UrlMapping;
+import main.java.utils.ClassManager;
+import main.java.utils.MethodManager;
 
 public class FrontControllerServlet extends HttpServlet {
     List<String> listClasses;
+    List<Method> listMethods;
 
     public void init() throws ServletException {
         String initial = this.getInitParameter("Controller");
         try {
-            listClasses = Utils.intoString(Utils.loadClasses(initial, Controller.class));
+            listClasses = ClassManager.intoString(ClassManager.loadClasses(initial, Controller.class));
+            listMethods = MethodManager.getAllMethods(initial);
         } catch(Exception e) {
             e.printStackTrace();
             listClasses = null;
@@ -38,21 +45,59 @@ public class FrontControllerServlet extends HttpServlet {
 
     private void processRequest(HttpServletRequest req, HttpServletResponse res) 
             throws ServletException, IOException {
-        res.setContentType("text/html;charset=UTF-8");
+        res.setContentType("text/plain;charset=UTF-8");
         String uri = req.getRequestURI();
         PrintWriter out = res.getWriter();
 
-        out.println("<html><body>");
-        out.println(uri);
-        printClasses(out);
-        out.println("<body><html>");
+        printClasses(uri, out);
+        out.println();
+
+        List<Method> showListFindMethods = new ArrayList<>();
+        List<Method> showListAllMethods = new ArrayList<>();
+        showListFindMethods = MethodManager.filterMethodWithEndPoint(listMethods, uri, UrlMapping.class);
+        showListAllMethods = MethodManager.filterMethodWithAnnotation(listMethods, UrlMapping.class);
+
+        if(showListFindMethods.isEmpty()) {
+            out.println("Aucun Method est associé à cette endpoint");
+            out.println();
+        } else {
+            Map<String, List<String>> mapTrouver = MethodManager.getInformationMethod(showListFindMethods);
+            out.println("L'information des methods associé à cette endpoint");
+            printMethods(mapTrouver, out);
+            out.println("===============================================");
+        }
+
+        out.println("Liste des methods existant avec l'annotation et ses informations");
+        Map<String, List<String>> map = MethodManager.getInformationMethod(showListAllMethods);
+        printMethods(map, out);
+
     }
 
-    private void printClasses(PrintWriter out) {
+    // private void filterRequest(String uri) {
+    //     if(uri.endsWith(".html") || uri.endsWith(".js") || uri.endsWith(".css") || uri.endsWith(".jsp")) {
+
+    //     }
+    // }
+
+    private void printClasses(String uri, PrintWriter out) {
+        out.println("Liste des classes :");
         for(String s : this.listClasses) {
-            out.println("\n");
-            out.println(s);
+            out.println("\t" + s);
         }
+    }
+
+    private void printMethods(Map<String, List<String>> map, PrintWriter out) {
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            String clazz = entry.getKey();
+            List<String> methods = entry.getValue();
+
+            out.println("Classe : " + clazz);
+            for (String m : methods) {
+                out.println("\t-> " + m + "()");
+            }
+            out.println();
+        }
+        out.flush();
     }
     
 }
