8f00e15c3cf28fca7b9098e4853349811634ef90
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-07-07 19:05:05 +0300

    initialized

--- src/main/java/listener/StartupListener.java
+++ src/main/java/listener/StartupListener.java
@@ -1,17 +1,40 @@
 package main.java.listener;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
+import main.java.annotation.Controller;
+import main.java.utils.ClassManager;
+import main.java.utils.MethodManager;
 
 public class StartupListener implements ServletContextListener {
     
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext context = event.getServletContext();
+
+        List<String> listClasses = new ArrayList<>();
+        List<Method> listMethods = new ArrayList<>();
+        String initial = context.getInitParameter("Controller");
+
+        try {
+            listClasses = ClassManager.intoString(ClassManager.loadClasses(initial, Controller.class));
+            listMethods = MethodManager.getAllMethods(initial);
+        } catch(Exception e) {
+            e.printStackTrace();
+            listClasses = null;
+        }
+
+        context.setAttribute("listClasses", listClasses);
+        context.setAttribute("listMethods", listMethods);
     }
 
     @Override
-    public void contextDestroyed(ServletContextEvent sce) {
+    public void contextDestroyed(ServletContextEvent event) {
 
     }
 
