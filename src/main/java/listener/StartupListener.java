f50fb334fdd9aaf0e6b4270b7e585c7328578b7f
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-07-07 18:42:29 +0300

    add context listener

---
+++ src/main/java/listener/StartupListener.java
@@ -0,0 +1,18 @@
+package main.java.listener;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class StartupListener implements ServletContextListener {
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+
+}
